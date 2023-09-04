a60bd56bd400f4d05af1bded92dea00f2b1f89a6
jlegacy-ai <jamalmuhammad.bsaif20@iba-suk.edu.pk>
2023-09-04 12:25:22 +0500

    Best Deal Section Added

--- src/app/common/BestDealSection.jsx
+++ src/app/common/BestDealSection.jsx
@@ -1,23 +1,178 @@
 "use client";
 import React, { useState } from "react";
+import Image from "next/image";
+import VisitScheduleButton from "./VisitScheduleButton";
+
+/*
+    Best Deal Villa Information Table Cell
+*/
+
+const BestDealTableCell = ({ value, description }) => {
+  return (
+    <div className="space-x-10">
+      <span className="text-tertiary-light-gray">{description}</span>
+      <span className="font-bold">{value}</span>
+    </div>
+  );
+};
+
+/*
+    Best Deal Villa Information Container
+*/
+
+const BestDealSubContainer = ({ data, description, title, imageURL }) => {
+  return (
+    <>
+      <div className="py-16 w-full flex justify-between space-x-10">
+        <div className="flex-1 shadow-lg flex flex-col justify-evenly items-center">
+          {data.map((e, i) => {
+            return (
+              <>
+                <BestDealTableCell {...e} />
+              </>
+            );
+          })}
+        </div>
+        <Image
+          className="flex-[2] shadow-lg"
+          src={imageURL}
+          width={500}
+          height={500}
+        />
+        <div className="flex-1 flex flex-col space-y-10">
+          <h5 className="text-normal-text font-bold">{title}</h5>
+          <div className="flex-1 flex flex-col justify-between">
+            {description.map((e, i) => {
+              return (
+                <p className="text-secondary-text-color" key={i}>
+                  {e}
+                </p>
+              );
+            })}
+          </div>
+          <VisitScheduleButton />
+        </div>
+      </div>
+    </>
+  );
+};
 
 /*
     Best Deal Button
 */
 
-const BestDealButton = ({ text, handler, selected }) => {
+const BestDealButton = ({ text, handler, selected, index }) => {
   return (
     <>
       <button
         className={`${
           selected ? "bg-secondary-orange " : "bg-black "
         } px-7  text-normal-text text-primary-whitesmoke py-3 mr-5 rounded-md`}
+        onClick={() => handler(index)}
       >
         {text}
       </button>
     </>
   );
 };
+
+/*
+    Best Deals Information
+*/
+
+const bestDealHouseInformationArray = [
+  {
+    data: [
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+    ],
+    imageURL: "/images/deal-01.jpg",
+    title: "Extra Info About Property",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.",
+      "When you need free CSS templates, you can simply type TemplateMo in any search engine website. In addition, you can type TemplateMo Portfolio, TemplateMo One Page Layouts, etc.",
+    ],
+  },
+  {
+    data: [
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+    ],
+    imageURL: "/images/deal-02.jpg",
+    title: "Extra Info About Property",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.",
+      "When you need free CSS templates, you can simply type TemplateMo in any search engine website. In addition, you can type TemplateMo Portfolio, TemplateMo One Page Layouts, etc.",
+    ],
+  },
+  {
+    data: [
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+      {
+        value: "185 m2",
+        description: "Total Flat Space",
+      },
+    ],
+    imageURL: "/images/deal-03.jpg",
+    title: "Extra Info About Property",
+    description: [
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.",
+      "When you need free CSS templates, you can simply type TemplateMo in any search engine website. In addition, you can type TemplateMo Portfolio, TemplateMo One Page Layouts, etc.",
+    ],
+  },
+];
+
 /*
     Best Deals Button
 */
@@ -62,11 +217,18 @@ const BestDealSection = () => {
                     key={i}
                     {...e}
                     selected={i === selectedButton}
+                    index={i}
+                    handler={setSelectedButton}
                   />
                 </>
               );
             })}
           </div>
+        </div>
+        <div className="flex justify-between">
+          <BestDealSubContainer
+            {...bestDealHouseInformationArray[selectedButton]}
+          />
         </div>
       </div>
     </div>
--- src/app/common/VisitScheduleButton.jsx
+++ src/app/common/VisitScheduleButton.jsx
@@ -3,11 +3,11 @@ import { FaCalendar } from "react-icons/fa";
 
 const VisitScheduleButton = () => {
   return (
-    <div className="h-10 flex justify-center items-center space-x-3 bg-tertiary-dark text-white pr-3 rounded-full group cursor-pointer">
+    <div className="h-10 flex justify-between max-w-[200px] items-center space-x-3 bg-tertiary-dark text-white pr-3 rounded-full group cursor-pointer">
       <span className="h-10 w-10 bg-secondary-orange flex justify-center items-center rounded-full -translate-x-1">
         <FaCalendar className="text-xl" />
       </span>
-      <span className="text-sm group-hover:text-secondary-orange transition-colors duration-500  ">
+      <span className="flex-1 text-center text-sm group-hover:text-secondary-orange transition-colors duration-500  ">
         Schedule a visit
       </span>
     </div>
